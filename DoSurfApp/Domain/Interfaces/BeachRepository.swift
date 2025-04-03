31c29db5c4afe3041f6ce1e1fadb007aa3cf3da3
piriram <pyoram25@gmail.com>
2025-04-03 22:36:51 +0900

    Refactor: 디자인 에셋 추가 및  폴더링

---
+++ DoSurfApp/Domain/Interfaces/BeachRepository.swift
@@ -0,0 +1,15 @@
+//
+//  BeachRepository.swift
+//  DoSurfApp
+//
+//  Created by 잠만보김쥬디 on 9/26/25.
+//
+
+import Foundation
+
+// MARK: - Repository
+protocol BeachRepository {
+    func findRegion(for beachId: String, among regions: [String], completion: @escaping (Result<String?, FirebaseAPIError>) -> Void)
+    func fetchMetadata(beachId: String, region: String, completion: @escaping (Result<BeachMetadata?, FirebaseAPIError>) -> Void)
+    func fetchForecasts(beachId: String, region: String, since: Date, limit: Int, completion: @escaping (Result<[ForecastData], FirebaseAPIError>) -> Void)
+}
