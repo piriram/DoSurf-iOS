ba2cb210c1d3810865fe65741e724ac087284d3b
piriram <pyoram25@gmail.com>
2025-04-04 11:54:14 +0900

    Feat: ChartDTO 생성

--- DoSurfApp/Domain/Interfaces/BeachRepository.swift
+++ DoSurfApp/Domain/Interfaces/BeachRepository.swift
@@ -11,5 +11,5 @@ import Foundation
 protocol BeachRepository {
     func findRegion(for beachId: String, among regions: [String], completion: @escaping (Result<String?, FirebaseAPIError>) -> Void)
     func fetchMetadata(beachId: String, region: String, completion: @escaping (Result<BeachMetadata?, FirebaseAPIError>) -> Void)
-    func fetchForecasts(beachId: String, region: String, since: Date, limit: Int, completion: @escaping (Result<[ForecastData], FirebaseAPIError>) -> Void)
+    func fetchForecasts(beachId: String, region: String, since: Date, limit: Int, completion: @escaping (Result<[FirestoreChartDTO], FirebaseAPIError>) -> Void)
 }
