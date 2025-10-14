b7293d5d3fb861f87c351d961c2ed0eb386ba80b
piriram <pyoram25@gmail.com>
2025-10-14 21:02:14 +0900

    Revert "Feat: 키보드 매니저 추가"
    This reverts commit dbbd8cd6842056075fe85907f2650fe18ac94bb1.

--- DoSurfApp/App/SceneDelegate.swift
+++ DoSurfApp/App/SceneDelegate.swift
@@ -6,26 +6,17 @@
 //
 
 import UIKit
-import IQKeyboardManagerSwift
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
 
-    private func configureIQKeyboardManager() {
-        IQKeyboardManager.shared.isEnabled = true
-        IQKeyboardManager.shared.resignOnTouchOutside = true
-        IQKeyboardManager.shared.keyboardDistance = 32
-        IQKeyboardManager.shared.resignOnTouchOutside = true
-    }
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        configureIQKeyboardManager()
         window = UIWindow(windowScene: windowScene)
         let vc = SurfSessionsTestViewController()
         window?.rootViewController = vc
