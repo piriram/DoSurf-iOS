be5f667798da5bac10569d859aa0225a3af3b7df
piriram <pyoram25@gmail.com>
2025-11-04 14:50:48 +0900

    Feat: SurfStartOverlayView 생성

--- DoSurfApp/Presentation/Scenes/TabBar/SurfStartOverlayView.swift
+++ DoSurfApp/Presentation/Scenes/TabBar/SurfStartOverlayView.swift
@@ -1,20 +1,20 @@
 //
-//  SurfEndOverlayView.swift
+//  SurfStartOverlayView.swift
 //  DoSurfApp
 //
-//  Created by 잠만보김쥬디 on 9/29/25.
+//  Created by 잠만보김쥬디 on 11/4/25.
 //
 
 import UIKit
 import RxSwift
 import RxCocoa
 
-// MARK: - Surf End Overlay View
-final class SurfEndOverlayView: BaseOverlayView {
+// MARK: - Surf Start Overlay View
+final class SurfStartOverlayView: BaseOverlayView {
     
     // MARK: - Callbacks
-    var onSurfEnd: (() -> Void)?
-    var onCancelSurfing: (() -> Void)?
+    var onSurfStart: (() -> Void)?
+    var onRecordDirectly: (() -> Void)?
     var onCancel: (() -> Void)?
     
     // MARK: - Initialization
@@ -33,26 +33,26 @@ final class SurfEndOverlayView: BaseOverlayView {
     // MARK: - Setup
     private func setupUI() {
         setupButtons(
-            main: .primary(title: "서핑 종료"),
-            secondary: .outlined(title: "서핑 취소하기", tintColor: .systemRed),
+            main: .primary(title: "서핑 시작"),
+            secondary: .outlined(title: "기록 바로하기", tintColor: .surfBlue),
             cancel: .icon(image: UIImage(systemName: "xmark"), tintColor: .surfBlue)
         )
     }
     
     private func bindActions() {
-        // 서핑 종료 버튼
+        // 서핑 시작 버튼
         mainButton?.rx.tap
             .throttle(.milliseconds(300), scheduler: MainScheduler.instance)
             .subscribe(onNext: { [weak self] in
-                self?.onSurfEnd?()
+                self?.onSurfStart?()
             })
             .disposed(by: disposeBag)
         
-        // 서핑 취소하기 버튼
+        // 기록 바로하기 버튼
         secondaryButton?.rx.tap
             .throttle(.milliseconds(300), scheduler: MainScheduler.instance)
             .subscribe(onNext: { [weak self] in
-                self?.onCancelSurfing?()
+                self?.onRecordDirectly?()
             })
             .disposed(by: disposeBag)
         
