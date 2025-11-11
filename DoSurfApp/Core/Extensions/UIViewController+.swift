8ea640b922533b538aa0caee4cfd9a1331aa4f14
piriram <pyoram25@gmail.com>
2025-11-11 17:29:36 +0900

    Refactor: 성능 개선을 위해 기록히스토리와 해변차트뷰의 구조를 변경

--- DoSurfApp/Core/Extensions/UIViewController+.swift
+++ DoSurfApp/Core/Extensions/UIViewController+.swift
@@ -4,18 +4,62 @@
 //
 //  Created by 잠만보김쥬디 on 10/31/25.
 //
-
 import UIKit
 
 extension UIViewController {
     
-    func showAlert(title: String, message: String) {
-        let alert = UIAlertController(
+    // MARK: - Action Sheet
+    func showActionSheet(
+        title: String? = nil,
+        message: String? = nil,
+        actions: [(title: String, style: UIAlertAction.Style, handler: (() -> Void)?)],
+        includeCancel: Bool = true
+    ) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+        
+        actions.forEach { action in
+            alertController.addAction(UIAlertAction(title: action.title, style: action.style) { _ in
+                action.handler?()
+            })
+        }
+        
+        if includeCancel {
+            alertController.addAction(UIAlertAction(title: "취소", style: .cancel))
+        }
+        
+        present(alertController, animated: true)
+    }
+    
+    // MARK: - Confirmation Alert
+    func showConfirmationAlert(
+        title: String,
+        message: String,
+        confirmTitle: String = "확인",
+        confirmStyle: UIAlertAction.Style = .default,
+        onConfirm: @escaping () -> Void
+    ) {
+        let alertController = UIAlertController(
             title: title,
             message: message,
             preferredStyle: .alert
         )
-        alert.addAction(UIAlertAction(title: "확인", style: .default))
-        present(alert, animated: true)
+        
+        alertController.addAction(UIAlertAction(title: confirmTitle, style: confirmStyle) { _ in
+            onConfirm()
+        })
+        
+        alertController.addAction(UIAlertAction(title: "취소", style: .cancel))
+        present(alertController, animated: true)
+    }
+    
+    // MARK: - Error Alert
+    func showErrorAlert(message: String) {
+        let alertController = UIAlertController(
+            title: "오류",
+            message: message,
+            preferredStyle: .alert
+        )
+        alertController.addAction(UIAlertAction(title: "확인", style: .default))
+        present(alertController, animated: true)
     }
 }
