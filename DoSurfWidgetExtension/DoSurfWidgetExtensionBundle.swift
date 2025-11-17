900591b277f858c0fcf4169f98dd79de12b160d7
piriram <pyoram25@gmail.com>
2025-11-17 12:10:53 +0000

    Fix: DoSurfWidgetExtensionBundle body에 SurfingLiveActivity 등록
    WidgetBundle의 body가 비어있어 라이브 액티비티가 시스템에 등록되지 않던 문제를 수정했습니다.
    SurfingLiveActivity를 body에 추가하여 라이브 액티비티가 정상적으로 작동하도록 했습니다.

--- DoSurfWidgetExtension/DoSurfWidgetExtensionBundle.swift
+++ DoSurfWidgetExtension/DoSurfWidgetExtensionBundle.swift
@@ -11,6 +11,8 @@ import SwiftUI
 @main
 struct DoSurfWidgetExtensionBundle: WidgetBundle {
     var body: some Widget {
-     
+        if #available(iOS 16.1, *) {
+            SurfingLiveActivity()
+        }
     }
 }
